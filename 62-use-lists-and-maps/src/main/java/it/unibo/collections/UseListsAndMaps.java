caac32fec2628c3ccc1b2fb4073480008d764dad
Marco Aresu <marcoaresu82@gmail.com>
2024-11-04 23:29:36 +0100

    first change uselistandmaps

--- 62-use-lists-and-maps/src/main/java/it/unibo/collections/UseListsAndMaps.java
+++ 62-use-lists-and-maps/src/main/java/it/unibo/collections/UseListsAndMaps.java
@@ -1,5 +1,7 @@
 package it.unibo.collections;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -9,14 +11,51 @@ import java.util.Map;
  */
 public final class UseListsAndMaps {
 
+    private final static int ELEM = 100000;
+
     private UseListsAndMaps() {
     }
 
     /**
      * @param s
-     *            unused
+     *          unused
      */
     public static void main(final String... s) {
+
+        ArrayList<Integer> arr = new ArrayList<>();
+        for (int i = 1000; i <= 2000; i++) {
+            arr.add(i);
+        }
+        System.out.println(arr);
+
+        LinkedList<Integer> list = new LinkedList<>(arr);
+        System.out.println(list);
+
+        int tmp; 
+        tmp = arr.get(0); 
+        arr.set(0, arr.get(arr.size()));
+        arr.set(arr.size(), tmp);
+
+        for(final int arrMember : arr){
+            System.out.println(arrMember);
+        }
+
+        arr.clear();
+        long timeArr = System.nanoTime();
+        for (int i = 1; i <= ELEM; i++) {
+            arr.add(i);
+        }
+        timeArr = System.nanoTime() - timeArr;
+        
+        list.clear();
+        long timeList = System.nanoTime();
+        for (int i = 1; i <= ELEM; i++) {
+            list.add(i);
+        }
+        timeList = System.nanoTime() - timeList;
+
+        System.out.println("Tempo array : " + timeArr + "\n" + "Tempo lista : " + timeList);
+
         /*
          * 1) Create a new ArrayList<Integer>, and populate it with the numbers
          * from 1000 (included) to 2000 (excluded).
